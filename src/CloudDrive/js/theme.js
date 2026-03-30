c46701afa506eff8dc08eb9120e1d7acfc2406f7
msy1332 <17322714918@163.com>
2026-03-30 21:27:33 +0800

    Modified some files

--- src/CloudDrive/js/theme.js
+++ src/CloudDrive/js/theme.js
@@ -1,37 +1,52 @@
-let ThemeButton = document.querySelectorAll(".top_nav .theme .button");
-let theme = "light";
-initTheme();
+let ThemeButton = document.querySelectorAll(".top_nav .theme button"); // 注意：通过querySelectorAll来获取的Dom元素是一个 NodeList的伪数组
+let currentTheme = "light"; // 当前主题
+initTheme(); // 初始化主题
 
-ThemeButton.forEach(function(btn){
-    dom.addEventListener('click',function(){
+// 通过 forEach循环来遍历dom集合并给点击事件绑定一个处理函数
+ThemeButton.forEach(function(btn){  
+    btn.addEventListener('click',function(){
         switchTheme();
     })
 })
 
 /**
  * 
- * @param {string} theme 
  * @returns {void} 无返回值 
  * @description 切换主题
  */
 function switchTheme()
 {
-    // theme
-    localStorage.setItem("theme",theme);
-    document.body.class = theme;
+    currentTheme = currentTheme === "light" ? "dark" : "light";
+    setTheme(currentTheme);
 }
 
 
 /**
  * 
- * @returns {string} 获取当前主题
+ * @returns {string} 初始化主题
  */
 function initTheme()
 {
-    theme = localStorage.getItem("theme");
-    if(theme)
+    currentTheme = localStorage.getItem("theme");
+    if(!currentTheme) currentTheme = "light";
+    setTheme(currentTheme);
+}
+
+/**
+ * 
+ * @param {string} theme 
+ * @description 设置主题
+ */
+function setTheme(theme)
+{
+    if(typeof(theme) === "string" && theme === "light" || theme === "dark")
     {
-        localStorage.setItem("theme","light");
-        theme = "light";
+        document.body.className  = theme;
+        localStorage.setItem("theme",theme);
+        currentTheme = theme;
+    }
+    else
+    {
+        throw Error("No theme!!");
     }
 }
